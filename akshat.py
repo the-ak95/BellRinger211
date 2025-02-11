dfceb66aaa077d9399d5434cff460b176ef4e9b9
Akshat Verma <157422041+the-ak95@users.noreply.github.com>
2025-02-11 13:43:36 -0800

    Create akshat.py

---
+++ akshat.py
@@ -0,0 +1,8 @@
+import pip
+import math
+pip install pytz
+timezone = pytz.timestone('US/Central')
+start = str(input("what is the time"))
+end = str(input("what time does class end"))
+
+difference = end - start
